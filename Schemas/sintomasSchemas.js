b7e8584828772708d8a46fcfb0c1e86dd89543ef
Yoltic2 <eyolticmg@icloud.com>
2023-04-10 10:08:26 -0600

    val

--- Schemas/sintomasSchemas.js
+++ Schemas/sintomasSchemas.js
@@ -3,16 +3,19 @@ const joi = require('joi');
 
 const sintoma_id = joi.string().uuid();
 const estadoSintoma = joi.boolean();
+const sintoma = joi.string();
 const valorIntensidad = joi.number().integer().min(1).max(10);
 const recordar =joi.boolean();
+const fr = joi.string();
 const notas = joi.string();
 const paciente_id = joi.string();
 
 const publishSintom  = joi.object({
-  sintoma_id : sintoma_id.required(),
+  id : sintoma_id.required(),
+  sintoma : sintoma.required(),
   intensidad:valorIntensidad.required(),
-  frecuencia:recordar.required(),
-  terminar:estadoSintoma.required(),
+  estado:estadoSintoma.required(),
+  frecuencia:fr.required(),
   notas:notas.required(),
   paciente_id:paciente_id.required()
 });
