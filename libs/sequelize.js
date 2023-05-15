96aa27b0c9a0dc0b6a6c87845b16f884fbc056a6
Yoltic2 <eyolticmg@icloud.com>
2023-05-14 20:24:24 -0700

    arreglando errores

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,9 +1,18 @@
-
+const { fa } = require('faker/lib/locales');
 const {Sequelize} = require('sequelize');
 
 const {config} = require('./../config/config');
 const setUPModels = require('./../db/models/index.js')
 
+
+// if(config.isProd){
+//   URI=config.isProd;
+// }else{
+
+// const user = encodeURIComponent(config.dbUser)
+// const password = encodeURIComponent(config.dbPassword)
+// const URI = `postgres://${user}:${password}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+// }
 
 const options = {
   dialect:'postgres',
