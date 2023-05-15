2f7d145f2290be17749fe0375ed3bc0e74f99b61
Yoltic2 <eyolticmg@icloud.com>
2023-05-14 21:39:40 -0700

    god plan

--- db/migrations/20230515043914-create-sintom-pacient.js
+++ db/migrations/20230515043914-create-sintom-pacient.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const {PacienteSchema,PACIENTE_TABLE} = require('../db/models/pacienteModel')
-const {SINTOM_TABLE,SintomaSchema} = require('../db/models/sintomasModel');
+const {PacienteSchema,PACIENTE_TABLE} = require('../models/pacienteModel')
+const {SINTOM_TABLE,SintomaSchema} = require('../models/sintomasModel');
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable(PACIENTE_TABLE,PacienteSchema);
